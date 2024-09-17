2974f59abec0f34a90612d6ce05deb46f497581b
charlesfonsecamorais <charlesfonsecamorais@gmail.com>
2024-09-17 12:36:36 -0300

    Criação do projeto e definição do padrão.

---
+++ src/main/java/br/com/boracodar/boracodar/BoracodarApplication.java
@@ -0,0 +1,13 @@
+package br.com.boracodar.boracodar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoracodarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoracodarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/boracodar/boracodar/BoracodarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boracodar.boracodar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoracodarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
